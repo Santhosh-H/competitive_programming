5f309ab4967f36126a86d3a05ab7e6888f46698f
Santhosh-H <santhoshhariharan1393@gmail.com>
2022-05-25 22:09:20 +0530

    insertation

--- Day1/answer.c
+++ Day1/answer.c
@@ -64,9 +64,7 @@ break;
  default:
 printf("\n Wrong Choice\n");
 break;
-}
-}
-}
+}}}
 void create()
 {
 struct node *temp,*ptr;
@@ -112,6 +110,91 @@ else
 printf("%d\t",ptr->info );
 ptr=ptr->next ;
  }
+}
+}
+void insert_begin()
+{
+struct node *temp;
+temp=(struct node *)malloc(sizeof(struct node));
+if(temp==NULL)
+{
+printf("\nOut of Memory Space:\n");
+return;
+}
+printf("\nEnter the data value for the node:\t" );
+scanf("%d",&temp->info);
+temp->next =NULL;
+if(start==NULL)
+{
+start=temp;
+}
+else
+{
+temp->next=start;
+start=temp;
+}
+}
+void insert_end()
+{
+struct node *temp,*ptr;
+temp=(struct node *)malloc(sizeof(struct node));
+if(temp==NULL)
+{
+
+printf("\nOut of Memory Space:\n");
+return;
+}
+printf("\nEnter the data value for the node:\t" );
+scanf("%d",&temp->info );
+temp->next =NULL;
+if(start==NULL)
+{
+start=temp;
+}
+else
+{
+ ptr=start;
+ while(ptr->next !=NULL)
+ {
+ptr=ptr->next ;
+ }
+ ptr->next =temp;
+}
+}
+void insert_mid()
+{
+struct node *ptr,*temp;
+int i,pos;
+temp=(struct node *)malloc(sizeof(struct node));
+if(temp==NULL)
+{
+printf("\nOut of Memory Space:\n");
+return;
+}
+printf("\nEnter the position for the new node to be inserted:\t");
+scanf("%d",&pos);
+printf("\nEnter the data value of the node:\t");
+
+scanf("%d",&temp->info) ;
+temp->next=NULL;
+if(pos==0)
+{
+temp->next=start;
+start=temp;
+}
+else
+{
+for(i=0,ptr=start;i<pos-1;i++)
+{
+ ptr=ptr->next;
+ if(ptr==NULL)
+ {
+printf("\nPosition not found\n");
+return;
+ }
+}
+temp->next =ptr->next ;
+ptr->next=temp;
 }
 }
 
