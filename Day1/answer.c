9a6b748df1212efcb279673ae2996e29040b4965
Santhosh-H <santhoshhariharan1393@gmail.com>
2022-05-25 21:57:32 +0530

    creatation and displaying

--- Day1/answer.c
+++ Day1/answer.c
@@ -67,5 +67,51 @@ break;
 }
 }
 }
+void create()
+{
+struct node *temp,*ptr;
+temp=(struct node *)malloc(sizeof(struct node));
+if(temp==NULL)
+{
+ printf("\nOut of Memory Space:\n");
 
+ exit(0);
+}
+printf("\nEnter the data value for the node:\t");
+scanf("%d",&temp->info);
+temp->next=NULL;
+if(start==NULL)
+{
+ start=temp;
+}
+else
+{
+ ptr=start;
+ while(ptr->next!=NULL)
+ {
+ptr=ptr->next;
+ }
+ ptr->next=temp;
+}
+}
+void display()
+{
+struct node *ptr;
+if(start==NULL)
+{
+ printf("\nList is empty:\n");
+ return;
+}
+else
+{
+ ptr=start;
+ printf("\nThe List elements are:\n");
+ while(ptr!=NULL)
+ {
+
+printf("%d\t",ptr->info );
+ptr=ptr->next ;
+ }
+}
+}
 
